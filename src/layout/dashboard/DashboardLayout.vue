7d215661ac65a1a981b55a139b1cd6772be3988e
konojunya <junya03dance@gmail.com>
2018-11-12 16:53:34 +0900

    init

---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link to="/dashboard" name="dashboard" icon="tim-icons icon-chart-pie-36"/>
+        <sidebar-link to="/table-list" name="storage" icon="tim-icons icon-puzzle-10"/>
+        <sidebar-link to="/table-list" name="server" icon="tim-icons icon-puzzle-10"/>
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click.native="toggleSidebar">
+      </dashboard-content>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu";
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+    MobileMenu
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  }
+};
+</script>
